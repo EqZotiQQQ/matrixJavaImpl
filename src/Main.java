aa2ad8751950d8a46276f44bc77d4877fcc7a044
Mikhail Fediakov <mfedya@mera.ru>
2019-11-06 15:42:54 +0300

    setMatrix

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Matrix mtx = new Matrix(3,3);
+        //  mtx.print();
+        mtx.set();
+        mtx.print();
+        mtx.multiply(5);
+        mtx.print();
+    }
+}
+
--- src/Matrix.java
+++ src/Matrix.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Matrix {
     private int mRows;
@@ -16,41 +17,59 @@ public class Matrix {
     }
 
     public void print() {
+        int x = 0;
         for(int[] rows : mMatrix) {
+            x++;
+            System.out.print("| ");
             for(int column : rows) {
                 System.out.print(column + " ");
             }
-            System.out.println();
+            System.out.println("|");
         }
     }
 
     public void set() {
-        for(int c = 0; c < mColums; c++) {
-            for(int r = 0; r < mRows; r++) {
-                Scanner sc = new Scanner(System.in);
-                String tmp = sc.nextLine();
-                String[] splittedInpult = tmp.split(" ");
-                //TODO: input like: 1 enter 2 enter 3 enter etc
-                //TODO: input like: 1 2 3 4 5 etc
-                //TODO: hybrid
+        String[] inputValues = enterValues();
+        fillMatrix(inputValues);
+    }
+
+    private String[] enterValues() {
+        int elemN = mColums*mRows;
+        String[] inputValues = new String[elemN];
+        for (int i = 0; i < elemN; ) {
+            Scanner sc = new Scanner(System.in);
+            String tmp = sc.nextLine();
+            tmp = tmp.replaceAll("[\\s]{2,}", " ");
+            String[] splittedInpult = tmp.split(" ");
+            for (int k = 0; k < splittedInpult.length; k++) {
+                if (i >= elemN) {
+                    break;
+                }
+                inputValues[i] = splittedInpult[k];
+                i++;
+            }
+        }
+        return inputValues;
+    }
+
+    private void fillMatrix(String[] inputValues) {
+        for (int c = 0, i = 0 ; c < mColums; c++) {
+            for (int r = 0; r < mRows; r++, i++) {
+                if (inputValues[i] == null) {
+                    continue;
+                }
+                mMatrix[c][r] = Integer.parseInt(inputValues[i]);
             }
         }
     }
 
     public int[][] multiply(int multiplier) {
         int[][] mtx = new int[this.mRows][this.mColums];
-        for(int c = 0; c < mColums; c++) {
+        for (int c = 0; c < mColums; c++) {
             for (int r = 0; r < mRows; r++) {
                 mtx[c][r] = this.mMatrix[c][r];
             }
         }
         return mtx;
-    }
-
-    public static void main(String[] args) {
-        Matrix mtx = new Matrix(3,3);
-        mtx.print();
-        mtx.set();
-        mtx.print();
     }
 }
