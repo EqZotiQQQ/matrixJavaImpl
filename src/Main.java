7d75e622865e67d7575f5933aa2900849290cebf
Mikhail Fediakov <mfedya@mera.ru>
2019-11-11 18:48:32 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,25 @@ public class Main {
     static Matrix D;
     static Matrix E;
     public static void main(String[] args) {
+        System.out.println("Matrix A:");
         A = new Matrix(3,3);
         A.set("matrix_square_1.txt");
         A.print();
 
+        System.out.println("Matrix B:");
+        B = new Matrix(3,3);
+        B.set("matrix_square_2.txt");
+        B.print();
+
+        System.out.println("Matrix C:");
+        C = new Matrix(2,4);
+        C.set("not_square_matrix_1.txt");
+        C.print();
+
+        System.out.println("Matrix D:");
+        D = new Matrix(4,2);
+        D.set("not_square_matrix_2.txt");
+        D.print();
     }
 }
 
